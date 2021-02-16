5071d52052283d8f0b129c61990d367ab35571ac
Moses <lazaromoses77@gmail.com>
2021-02-16 22:59:45 +0300

    changing moon_function

--- src/Nav.js
+++ src/Nav.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 class Nav extends Component {
     state = {}
+
+    changeTheme = () => {
+        const moon = document.querySelector('.fa-moon');
+        moon.addEventListener('click', () => {
+            document.body.classList.toggle('dark-theme');
+        })
+    }
+
     render() {
         return (
 
@@ -15,6 +23,11 @@ class Nav extends Component {
                         <Link className="link" to="/Recipes">
                             <li>Recipes</li>
                         </Link>
+                        <li className="link">
+                          <span>
+                             <i className="fa fa-moon" onClick={this.changeTheme}></i>
+                          </span>
+                        </li>
                     </ul>
                 </nav>
             </main>
